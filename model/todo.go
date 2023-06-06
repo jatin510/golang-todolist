fe66e341fd8268fdd0bbde452f8c309e5cc977dc
Jagdish Parihar <jatin6972@gmail.com>
2023-06-07 01:40:20 +0530

    udated the todo model

--- model/todo.go
+++ model/todo.go
@@ -1,12 +1,15 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Todo struct {
-	Id        string    `json:"id"`
-	Task      string    `json:"task"`
-	User      string    `json:"user"`
-	Completed bool      `json:"completed"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
+	Id        primitive.ObjectID `bson:"_id" json:"id"`
+	Task      string             `bson:" task" json:"task"`
+	Completed bool               `bson:"completed" json:"completed"`
+	CreatedAt time.Time          `bson:"createdAt" json:"createdAt"`
+	UpdatedAt time.Time          `bson:"updatedAt" json:"updatedAt"`
 }
